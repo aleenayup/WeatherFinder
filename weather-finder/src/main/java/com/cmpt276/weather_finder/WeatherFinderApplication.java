8be8a7e6c66a096b8c7f56e9ba4715e4ada0d8fa
aleenayup <gulaleena06@gmail.com>
2026-06-05 22:05:30 -0700

    basic buttons

---
+++ weather-finder/src/main/java/com/cmpt276/weather_finder/WeatherFinderApplication.java
@@ -0,0 +1,13 @@
+package com.cmpt276.weather_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherFinderApplication.class, args);
+	}
+
+}
---
+++ weather-finder/src/test/java/com/cmpt276/weather_finder/WeatherFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpt276.weather_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
